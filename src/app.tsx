567beaa93ecb6709400b046ad5195f6ea908d706
Ihor Bozhuk <88133512+Jotunn1@users.noreply.github.com>
2024-09-07 21:03:10 +0300

    create queryClient

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,13 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 export function App() {
   return (
-    <div>
-      <h1 style={{ color: "red" }}>hello from preact</h1>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <h1 style={{ color: "red" }}>hello from preact</h1>
+      </div>
+    </QueryClientProvider>
   );
 }
